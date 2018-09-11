0684d19b39cc11ee399417884e4a53524b1bb24f
Gustavo <GustavoLopes899@gmail.com>
2018-09-11 14:12:10 -0300

    list 01 - exercise 04

---
+++ src/lista01/exercicio04/Exercicio04.java
@@ -0,0 +1,13 @@
+package lista01.exercicio04;
+
+public class Exercicio04 {
+    
+    public static void main(String[] args) {
+        final int tam = 10;
+        int vetor[] = new int[tam];
+        for (int i = 1; i < tam; i++) {
+            
+        }
+    }
+
+}
